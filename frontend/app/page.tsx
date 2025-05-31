530586d2610719d096d33f1d83e13933b22709aa
tomasgrusz <tomas@grusz.dev>
2025-06-01 00:23:48 +0200

    fix: world id verifcation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,40 +14,39 @@ const verifyPayload: VerifyCommandInput = {
   verification_level: VerificationLevel.Orb, // Orb | Device
 };
 
-const handleVerify = async () => {
-  if (!MiniKit.isInstalled()) {
-    return;
-  }
-  // World App will open a drawer prompting the user to confirm the operation, promise is resolved once user confirms or cancels
-  const { finalPayload } = await MiniKit.commandsAsync.verify(verifyPayload);
-  if (finalPayload.status === "error") {
-    return console.log("Error payload", finalPayload);
-  }
-
-  // Verify the proof in the backend
-  const verifyResponse = await fetch("/api/verify", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      payload: finalPayload as ISuccessResult, // Parses only the fields we need to verify
-      action: "sign-in",
-      signal: "0x12312", // Optional
-    }),
-  });
-
-  const verifyResponseJson = await verifyResponse.json();
-  if (verifyResponseJson.status === 200) {
-    alert("Verification successful!");
-    redirect("/home");
-  } else {
-    localStorage.setItem("isVerified", "false");
-    alert("Verification failed. Please try again.");
-  }
-};
-
 export default function Home() {
+  const handleVerify = async () => {
+    if (!MiniKit.isInstalled()) {
+      return;
+    }
+    // World App will open a drawer prompting the user to confirm the operation, promise is resolved once user confirms or cancels
+    const { finalPayload } = await MiniKit.commandsAsync.verify(verifyPayload);
+    if (finalPayload.status === "error") {
+      return console.log("Error payload", finalPayload);
+    }
+
+    // Verify the proof in the backend
+    const verifyResponse = await fetch("/api/verify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        payload: finalPayload as ISuccessResult, // Parses only the fields we need to verify
+        action: "sign-in",
+        signal: "0x12312", // Optional
+      }),
+    });
+
+    const verifyResponseJson = await verifyResponse.json();
+    if (verifyResponseJson.status === 200) {
+      alert("Verification successful!");
+      redirect("/home");
+    } else {
+      alert("Verification failed. Please try again.");
+    }
+  };
+
   return (
     <div
       onClick={handleVerify}
