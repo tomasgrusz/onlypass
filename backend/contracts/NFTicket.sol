7532fccd0a50ab5be7b396f92ea659be63c26471
Patrik Matula <73120426+PatMatSK@users.noreply.github.com>
2025-05-31 15:54:16 +0200

    Update NFTicket.sol

--- backend/contracts/NFTicket.sol
+++ backend/contracts/NFTicket.sol
@@ -8,15 +8,16 @@ contract NFTicket is ERC721 {
     uint256 public tokenCounter;
 
     struct NFTDetails {
-        string location;   // adresa
-        string date;       // dátum (ako string, prípadne môžeš použiť uint256 ak chceš timestamp)
-        string title;       // názov
+        string location;   
+        string date;    
+        string title;
+	string tokenHash;
         string signature;
     }
 
     mapping(uint256 => NFTDetails) private _tokenDetails;
 
-    constructor() ERC721("MyNFT", "MNFT") {
+    constructor() ERC721("NFTicket", "NFTi") {
         tokenCounter = 0;
     }
 
@@ -25,10 +26,11 @@ contract NFTicket is ERC721 {
         string memory location,
         string memory date,
         string memory title,
+	string memory tokenHash,
 	string memory signature
     ) public returns (uint256) {
         _safeMint(to, tokenCounter);
-        _tokenDetails[tokenCounter] = NFTDetails(location, date, title, signature);
+        _tokenDetails[tokenCounter] = NFTDetails(location, date, title, tokenHash, signature);
         tokenCounter++;
         return tokenCounter - 1;
     }
@@ -37,9 +39,10 @@ contract NFTicket is ERC721 {
         string memory location,
         string memory date,
         string memory title,
+	string memory tokenHash,
 	string memory signature
     ) {
         NFTDetails memory details = _tokenDetails[tokenId];
-        return (details.location, details.date, details.title, details.signature);
+        return (details.location, details.date, details.title, details.tokenHash, details.signature);
     }
 }
