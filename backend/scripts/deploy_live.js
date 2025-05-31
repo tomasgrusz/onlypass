2a605cbe84c30708b2b1723b9c8158ded7057aab
Patrik Matula <73120426+PatMatSK@users.noreply.github.com>
2025-05-31 19:42:29 +0200

    Update deploy_live.js

--- backend/scripts/deploy_live.js
+++ backend/scripts/deploy_live.js
@@ -5,7 +5,7 @@ const { ethers } = hre;
 async function main() {
 
   const provider = ethers.provider;
-  const wallet = new ethers.Wallet(<PRIVATE-KEY>, provider);
+  const wallet = new ethers.Wallet(<PRIVATE-KEY>, provider); // 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
   const NFTt = await hre.ethers.getContractFactory("NFTicket", wallet);
   const NFTi = await NFTt.deploy();
