de0e097c0dde411309e345726c0e203264ca4b54
Patrik Matula <73120426+PatMatSK@users.noreply.github.com>
2025-05-31 14:48:02 +0200

    Add files via upload

---
+++ NFTicket.sol
@@ -0,0 +1,43 @@
+// contracts/MyNFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTicket is ERC721 {
+    uint256 public tokenCounter;
+
+    struct NFTDetails {
+        string location;   // adresa
+        string date;       // dátum (ako string, prípadne môžeš použiť uint256 ak chceš timestamp)
+        string name;       // názov
+    }
+
+    mapping(uint256 => NFTDetails) private _tokenDetails;
+
+    constructor() ERC721("MyNFT", "MNFT") {
+        tokenCounter = 0;
+    }
+
+    function createNFT(
+        address to,
+        string memory location,
+        string memory date,
+        string memory name
+    ) public returns (uint256) {
+        _safeMint(to, tokenCounter);
+        _tokenDetails[tokenCounter] = NFTDetails(location, date, name);
+        tokenCounter++;
+        return tokenCounter - 1;
+    }
+
+    function getNFTDetails(uint256 tokenId) public view returns (
+        string memory location,
+        string memory date,
+        string memory name
+    ) {
+        NFTDetails memory details = _tokenDetails[tokenId];
+        return (details.location, details.date, details.name);
+    }
+}
+
