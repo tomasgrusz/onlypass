a24a2050c68b7110cd2da05db27db5f6f349cfea
Patrik Matula <73120426+PatMatSK@users.noreply.github.com>
2025-05-31 21:51:48 +0200

    Update deploy_live.js

--- backend/scripts/deploy_live.js
+++ backend/scripts/deploy_live.js
@@ -10,42 +10,10 @@ async function main() {
   const NFTt = await hre.ethers.getContractFactory("NFTicket", wallet);
   const NFTi = await NFTt.deploy();
   
-  // const deployedAddress = "0x457cCf29090fe5A24c19c1bc95F492168C0EaFdb";  // flow address
-  // const NFTi = NFTt.attach(deployedAddress);
-
-  // await NFTi.deployTransaction.wait();
+  await NFTi.deployTransaction.wait();
 
   console.log("NFTicket deployed to:", NFTi.address);
-
-  // -------------------------------------- DETAILS ---------------------------------------
-  const ticketOwner = wallet.address // "0x5fbdb2315678afecb367f032d93f642f64180aa3"
-  const location = "Berlin";
-  const date = "31-02-3063";
-  const title = "Elvis.P";
-  const tokenHash = "0xffeeddccbbaa99887766554433221100";
-  const signature = "0x00112233445566778899aabbccddeeff";
-  // --------------------------------------------------------------------------------------
-
-  // const tx = await NFTi.createNFT(ticketOwner, location, date, title, tokenHash, signature);
-  // await tx.wait();
-
-  console.log("NFT minted to:", ticketOwner);
-
-  for (let tokenID = 0; tokenID <= 2; tokenID++) {
-    try {
-      const details = await NFTi.getNFTDetails(tokenID);
-
-      console.log("Details of tokenId ", tokenID);
-      console.log("Location:", details.location);
-      console.log("Date:", details.date);
-      console.log("Name:", details.title);
-      console.log("tokenHash:", details.tokenHash);
-      console.log("Signature:", details.signature);
-    } catch (err) {
-      console("Error!");
-    }
-  }
-
+  
 }
 
 main().catch((error) => {
