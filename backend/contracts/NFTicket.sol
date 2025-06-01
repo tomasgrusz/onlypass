615794718f9c13b25e51791264b3cbe4152caa54
Patrik Matula <73120426+PatMatSK@users.noreply.github.com>
2025-06-01 08:41:41 +0200

    Update NFTicket.sol

--- backend/contracts/NFTicket.sol
+++ backend/contracts/NFTicket.sol
@@ -3,6 +3,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+using Strings for uint256;
 
 contract NFTicket is ERC721 {
     uint256 public tokenCounter;
@@ -16,21 +19,24 @@ contract NFTicket is ERC721 {
     }
 
     mapping(uint256 => NFTDetails) private _tokenDetails;
+    mapping(uint256 => string) private _tokenMetadataURIs;
 
     constructor() ERC721("NFTicket", "NFTi") {
         tokenCounter = 0;
     }
 
-    function createNFT(
+    function createNFTicket(
         address to,
         string memory location,
         string memory date,
         string memory title,
 	string memory tokenHash,
-	string memory signature
+	string memory signature,
+        string memory metadataURI
     ) public returns (uint256) {
         _safeMint(to, tokenCounter);
         _tokenDetails[tokenCounter] = NFTDetails(location, date, title, tokenHash, signature);
+        _tokenMetadataURIs[tokenCounter] = metadataURI;
         tokenCounter++;
         return tokenCounter - 1;
     }
@@ -45,4 +51,10 @@ contract NFTicket is ERC721 {
         NFTDetails memory details = _tokenDetails[tokenId];
         return (details.location, details.date, details.title, details.tokenHash, details.signature);
     }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+      return _tokenMetadataURIs[tokenId];
+    }
+
 }
+
